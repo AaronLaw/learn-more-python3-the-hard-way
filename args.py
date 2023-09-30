a102d0a0d1f5df2720342bea3e158f671baa361a
Aaron Law <aaronlaw@gmail.com>
2023-09-30 16:17:52 +0800

    wrap the code into functions.

--- args.py
+++ args.py
@@ -2,23 +2,30 @@
 import sys
 from typing import List
 
-args: List = sys.argv
-print(args)
-print(f'The file itself is: {args[0]}')
+def with_argv():
+    args: List = sys.argv
+    print(args)
+    print(f'The file itself is: {args[0]}')
 
 
 # Here we use `argparse` to get argument from stdin
 import argparse
 
-parser = argparse.ArgumentParser(description="To sum up 2 numbers")
-parser.add_argument("-v", "--verbose", help="increase output verbosity",
-                    action="store_true")
-parser.add_argument("first", help="The first number",
-                    type=int)
-parser.add_argument("second", help="The second number",
-                    type=int)
+def with_argparse():
+    parser = argparse.ArgumentParser(description="To sum up 2 numbers")
+    parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                        action="store_true")
+    parser.add_argument("first", help="The first number",
+                        type=int)
+    parser.add_argument("second", help="The second number",
+                        type=int)
 
-args = parser.parse_args()
-if args.verbose:
-    print(f"this is from args: {args.first} + {args.second}")
-print(args.first + args.second)
+    args = parser.parse_args()
+    if args.verbose:
+        print(f"this is from args: {args.first} + {args.second}")
+    print(args.first + args.second)
+
+
+if __name__=="__main__":
+    with_argv()
+    with_argparse()
