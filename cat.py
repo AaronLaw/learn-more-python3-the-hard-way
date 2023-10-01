cce7a8c4ae87667333469471f0da9b56590ad2e9
Aaron Law <aaronlaw@gmail.com>
2023-10-01 22:08:12 +0800

    1st draft of cat.

--- cat.py
+++ cat.py
@@ -1,1 +1,25 @@
-print(f'this is cat')
+# A implementation of `cat` in Python
+
+"""
+# some functions of `cat`:
+
+## print the contents of a file to stdout
+cat file
+
+## concatnate files into an output file
+cat file1 file2 file3 > outfile
+
+## append files into an output file
+cat file1 file2 file3 >> outfile
+
+## number the output lines
+cat -n file
+"""
+
+def cat():
+    with open('text.txt', 'rt') as f:
+        for line in f:
+            print(line)
+
+if __name__=="__main__":
+    cat()
