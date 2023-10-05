759142cc3a8ecbb23ab222e3cb878b3eafb32167
Aaron Law <aaronlaw@gmail.com>
2023-10-06 02:29:47 +0800

    more generic.

--- bookmark_to_md.py
+++ bookmark_to_md.py
@@ -23,20 +23,27 @@ class BookmarkToMarkdown:
         for item in bookmark_list:
             # print(type(item))
             stem: str = self.get_filename(item)
-            url: str = self.get_url(item)
+            contents: List[str] = self.get_contents(item)
+            url: str = self.get_url(contents)
             print(f"- [{stem}]({url})")
             # self.write_markdown()
 
-
     def get_filename(self, path) -> str:
-        """Get the filename part from a Path.
+        """Return the filename part from a Path.
         """
         return path.stem
 
-    def get_url(self, path) -> str:
+    def get_contents(self, path) -> list[str]:
+        """Given a path and return its contents.
+        """
         with open(path, 'r') as file:
-            url = file.readlines()[self.line_number-1].rstrip()[4:]
-            return url
+            return file.readlines()
+
+    def get_url(self, lines) -> str:
+        """Given a content and return a line.
+        """
+        url = lines[self.line_number-1].rstrip()[4:]
+        return url
 
     def write_markdown(self) -> None:
         pass
