bf0736ec30447a3856f2f313176bf89659eda7d0
Aaron Law <aaronlaw@gmail.com>
2023-10-06 15:51:42 +0800

    make it as a Command.

--- bookmark_to_md.py
+++ bookmark_to_md.py
@@ -1,11 +1,19 @@
 from typing import Union, Any, Optional
 from Command import BookmarkToMarkdown
 
+import click
+
+@click.command()
+@click.argument('path', type=click.Path(exists=True))
+@click.option('-o', '--output', default='bookmarks.md', help='The output filename')
+def bookmark_to_markdown(path: click.Path, output: str):
+    """Convert bookmark files to a single markdown file.
+    """
+    # path = '/home/aaron/Music/reload/'
+    fetch_line_number = 2
+    file_suffix = "*.URL"
+    # out_filename = "bookmarks.md"
+    BookmarkToMarkdown(path, fetch_line_number, file_suffix, output).execute()
 
 if __name__=="__main__":
-    path = '/home/aaron/Music/reload/'
-    fetch_line_number = 2
-    file_end = "*.URL"
-    out_filename = "bookmarks.md"
-
-    BookmarkToMarkdown(path, fetch_line_number, file_end, out_filename).execute()
+    bookmark_to_markdown()
